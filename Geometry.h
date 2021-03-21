4cd13d65801bee75dd14cda4fb86190555452ad7
Seungwoo Yoo <dreamy1534@gmail.com>
2021-03-21 21:24:32 +0900

    feat(main): Initialize Repo

---
+++ Geometry.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/**
+* Base class for geometries in the project
+*/
+class Geometry {
+public:
+
+private:
+};
---
+++ main.cpp
@@ -0,0 +1,14 @@
+// C++ standarad libraries
+#include <iostream>
+
+// OpenGL libraries
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+// Utilities
+#include "glsupport.h"
+#include "ppm.h"
+
+int main() {
+	
+}
